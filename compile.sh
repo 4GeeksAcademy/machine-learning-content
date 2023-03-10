d53be2aa44d105dbd7c1c48c2eef89028e4c2c76
Alejandro Sanchez <aalejo@gmail.com>
2023-03-10 17:04:42 +0000

    no more export

---
+++ compile.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+rm -rf export
+
+# loop over all notebooks and run nbconvert over them
+for i in $(find . | grep -F .ipynb); do
+  jupyter nbconvert --to markdown $i
+done
+
+# mkdir export
+# cp ./*/*.md ./export
+# cp ./*/*.ipynb ./export
--- export/time-series.es.md
+++
@@ -1,3 +0,0 @@
-Fuente: 
-
-https://towardsdatascience.com/how-to-forecast-time-series-with-multiple-seasonalities-23c77152347e
